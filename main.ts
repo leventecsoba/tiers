31efccbcf8802f22a8a2bbb1718fe3788b51912c
leventecsoba <csobal26@gmail.com>
2025-07-28 09:00:47 +0200

    yes

--- main.ts
+++ main.ts
@@ -1,215 +1,182 @@
-const MAX_TICK_COUNT = 10
-const TICK_LENGTH = 10
-const SVG_HEIGHT = 500
-const BAR_WIDTH = SVG_HEIGHT / 10
-const VALUE_INDICATOR_COLOR = "#d6d6d6"
-const DEFAULT_ELEMENT_PADDING = 10
+let isDragging: boolean = false
+let draggedElementObjectURL: string | null = null
 
-interface BarChartData {
-    label: string
-    value: number
-}
+const PEEPO_CLAP = "R0lGODlhOAA2APeDAAABEAACHQEDIgMGAQAFKAAGMQUJBAEKPgAMRQAQTQcOTiAOAgARVQoWAgAUXggUXygTBAAXaBcYFmMAIg8dBQoZcnIAJy8YBwAdexMhDBUiBR0eHHwAKwAghQAijjUdBxonDRcpBzseDJEANgAmmCUmJBksCxgkbpwBOzIlKxEmmgMqpKgAPUIkDQAtrLIAQxw0DQAvt0oqDyQ2FwAzwcYATTAzMAE1zCM7FCU7DlMuFMoFUEAyOSc+GAA71zc5NypBFCVDFNcIUQY94l01FgBB7QBD9y1KFWU6FjJJIwBG/20+GlVCSkVIRjRRHHVEGjVYGnhHHU5QTjlbHTxeIH1LIT5eKYJKI1VXVT9iJIZNH4lPIkNnKI1SJXFXYkJrJVBnQ0ZvKEpyLGRnZXtga2FtXFJ2O018LYVpdFOCMlp/RF+DSVuJM1aLM5JzgHp9elqQOKmEkouOjLGMmpSWlLqToqWmqqWnpKyvq6uvsrS3ur3AvMbIxc/S1eTm4+nr6PDy7/T38/3//DUADj4gBzI8LE46QmRPWJ97icebq8+ist2vwNfa3Q0GBBcLBUEAFA8RDkkAGAAacCgbIocAMCstK70ASD5BPj1UJkNbN0VmMV1fXUxuNFNyQGxubGJ7UHN1c12GPm+MYYOGhJyfnbS3s7y/ws/Sz9fa1t7g3d/j5vj79x8ACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh+QQFCgCDACwAAAIAOAA0AAAI/wAHCRxIsODAAQgRGiw4wIABhQsjSow4gIKJGT1ymKAwYKEBDTOA9DChQYOBiSglGghB5Uyal2eyANHQ4GFDCj24pGGTJgwXKk40dExJdFADGF/awFnahk2bnk40mgjC5UybM1+oUMnyRQyVEEVTGuhhlWmaM2LChBHj8ozLq1mmsIXJJYyToWEX4izLBisXtmh/ftnZ5ksWq02bwnRCoQHevAIbkF3a8zCby5dfGrYqhg1lMT+1TnFyBMZJyANBiFkKR4yYp2KyZNG6lcuXM06bes2y1gwnTpqsOIGBWqAGKkqXOs0KBYoTJ2U8SS9TRk3f2ZzKjCJF6k4pPHI8Zf8iHhZhgymelcec7RzMmzt8Uvnxc+pOGSpfytw5lQoPqDd7/AFIKqWMUh4FFByRBmtwpBHXFFyAsd0om4CiRyCCCBLIHhL2EcgqpWDxxiV08LGKIKuc8kZRGkCBw2qsseEVF2a8UQoqb3jyRhOknJKhIIDQgUqGfoxxyShS2CHHHxkGQgpRBhzBhYIxcpEFJ52QksoqfPywiSebkCIHhiiWwqQgp/wASSWgSHHJkCg+mZIJYjjBRXJwsGEngIBkiMcGBjTxhpdnAoJHn4LwYcMAkGBRSSVworKJWE6cEcQZDF6lyR4n+inBACVcYsAmTAZiCqcZoiJFQgNIkcofp4z/skFKIbg1BZ6stTEKKmQqmpABowCiSh5lkMLrKoDI8SlCY5RCxydimIDSAEc0lR6DyomSByp//PEGJAjZUAofo3SSRieekMIHKqVc0kAGQGSyhmZgTUTBF9jmq9gncoBXCYJHfpKGUoqZIYooZqg1cBpfAPHBByjVmi+2WInxUihqpNXVwBO34bFiXxwhwxJXPCGCRNTimm8amCyBBCZ/7YTZxEyBjIMJEESxRRQ6LCBRA2HQzJQYT3ShRRRLEIEJJlSE8RJmmL0EWlQaTHEGEDrocEEjE8GwoNBtcFHFFTq0oAMSTyStgwxJtL102zjAEIIMRIgAxMBQONLIYwZF/6kytm1QgQQSFrCAAgcTEIHEEk807vgTRMzwhRMftDCDE2rhkAMIfBfUQBZ/Z/rFyBbUIMQOlqBACeJEtL62DpHksNMXUxwBBBAwTJEGFxRM1ACMQsNxxgVL6MCCEMgjj/oLLDTf/AgiWMVGFpVebdUZ9UakAabBNwjEEkuUnvz44+9gARBHqHUEpmn0AIVrGUxkwtfBh60D2RyYTv74lmDC8BE4QE7YYBAVIDhGIjigX/DS4IQlVEEH+VOeJSZYgx0IoQZIcMpZFiQGIIRBT0GgAkciMr/useYMSXhCFYhgAeZZgAhMQMIEKDECFEyAe0w5AhWcgp80zKBzkQmaCf+XgkIHIiESj0iBG+ZQhzi4AQ1e4EEY8NQGp8kIB2HA3s+gELqOoZAIUYgCEnTABDLEYQ5xSKMXppgpB5kgBF8IwmkWMoAZKLB7V6GCDJAwtieMUQY8COQkQJepLMAAKCGY40IAcAAuXGuIbRBDEvY4titYEgkHSAAMcAiHq1yPAgGYyAFc8ABHDjFXUJlBCxSHhAfQgAYMuFWmPCaGCLhAIgDAQBFuUAH/dZFmTzkDFUwggAjcwAhGcEEJOEnLS9ygCEWISAFcoAQj+MADPxiMx04ZSSAggARDMIISlFCECBCSJ3Z5gAvEaYSIKMAH4yRnDE7wgyxYbJvATMMUKhH/ARoUIZ7VJIGCzjAFIGRglD6A5hBosBBJiDOeRhiCCzDwABu8jzCoPAMXfoABF4QToONcgQRmUAIHeKADEaiASlXKgIV44KEgLYIPaNABBlwCZmvBShIY0IEYfBSkRriBJEgQgxg8U6Y+uIFSaSAJABiEmiCN6UJXUIEDwICfK0goTCE6BKrG4J9RBWgMCGAQGoQ1rBHtQEd/GlWJVgADNNjqWYfgAIPc4KxzZStAIxoDDFTAp3I9qxE6wLe74vWwe3XrWgOLVyOQgG9mRexhI+oClQJWsjHFAN9igNmw7pIEklhsZwHa1QM8dbTjjCgNPPAAD9BAr50tAk0L0LkOjYC1sz7AQARIkFDUQvQGHjgAAICIAI/eFrFdPaZvyZnUFTCAthMpgCQ8cFnEMhaiyCzCELZ7AxqswAMROEABnEoUACBAEiuggVYlG9HtDiGpMXCBCirggAh0QBIPOEAoi8PIA5h0BS4oqoCLSoOlzvcBDGAAfRFwAP0CsTgGIUABJkzhAhxAAXWFMFECAgAh+QQFCgCNACwBAAMANwAyAAAI/wAbCRxIsKDAEAMRGiSIo0cPGBoyLJxIsSJBKkAkCjTQoEHBMxZDTjQQEofCRmkIZqHCRUwWkTAnhvlCME3KgjcFiokZs03BnQTh8Bw6kGRINmnCZLnpcyEVJ05yEJ36cqBQgmayToU5ZcpAMI3KjClIparAO3b04KEjEMRWg1Qm/qHoJ8/CN29F7uEpaOoMnlIa8SnIiOAPOXkNNg2jhqCNioAM4k0cUoIBKW9sYIk8MBBlgVCIbvhhYDPBvgKbwOz4uaAfPEQHNALC5QgFCgOdFGyTpulAPH0WwraIu6AGAyYaOGkzJYtugWHgSIeTxskRKGfChGFTkK3FxhUvZP/gguNL9eoGkzqBkkYMECA9zKJcI3I6nKZJCM44c4bNmSlsAIETVDhM0UYbTojxRQtICCQfT0e0sIQWA+XQWxv8nfHcQGJAgeERvVGRBkhvJVGIDEhUccUTA82QhnRssBEGiRx6CEcYXMAhhowCyRBTEjoQscQSTzyxhA6EtHDBQWdM1wYbvgmURg5fHMjFhTQRZIEOBEVR5JdgIkGEDoYw4UULUSyxQCMacNGGfdzV1MMRSx3ooEU65DmQDjJ88MEhaCBShyKJHFLFFh80MgAOL05Ho5RAZEfFm9J9AUFML7zAwgQpoKHIIqAuQsYTXRDSyAEPTHHGgWdkVFMQB/b/Nt0XRokkxK01oDABIqEqwsShS0ZwAwY/ZBFGDrVKeYQY9kkHxQAyUBjSrdSiQAaoirihQxdRQAAACUYMEcMDGSQ7UG2USnfGDAG4ZSu1O4zgaR1k6FBFFzpAEQANSihhRAcATJQGbZSm0QMGGBwhkBZc7skEExNwIBALLKDAwSFuoMEEEVpssYQVaSgwRL9DREAQs/aJEUSVaSQhLA2FAHWFQINM9DATZJCx8aFbIAHBF2FgYES/PhRQURtf5ADEDysMYUQREQDViIpIyHAIGYgINEcccSCCxiFIRNFFF1XoMAMFX/SgQr9KrBAAQVLt5l4FRfRrxApWDMTgFVcs/2EQD4YQEcUWW1SBhAhOhGFCCATwqwTUMJ2RwA1s+6BATo20YNAVVUTh+RJEyNBCEmIkdRsDIytxwwEwIejB0P56kOVEQ45JyAVAQNFSb2L8gEEBQvfr9kInLCRGAj6wTUMSUUIHRRAOHUHFjLJS98UDMRSxggOUG1EBT20kEQPbRVQQXbMHxgjlfdLxFsYPHdwwdBEkYDDEEA4M9cUJdfcbgxPNCuB9eKMhG1Qge2x7HAZcEAPW8eQLJRgfyRjQqAASMAxQKMQDOhADpyXQfxGoQMB4ooAD0M1uHciCGFa4wjOIQSlJOAADSEADH/Tvg/0qAgYEsBUOKq8ACniAEGqHeIIOuKCGsMPhB2nwlmH1rwgdWIEPpkhFDyawNRPRoeOUMIQbKrFfWKSICjqQxC+yLYwVuYEDkmdGMKKxIlB0QRvfKBIakFGJbyFAYorggzLmBQGtuSIdt6KEHuZlCG8swAgHSREFtCYgADs="
 
-type Position = "left" | "right" | "top" | "bottom"
-
-interface ElementOptions {
-    label?: string
-    labelPosition?: Position
-    hexColor?: string
-}
-
-type LineOptions = ElementOptions
-type RectangleOptions = ElementOptions
-
-const BAR_CHART_DATA = [
-    {value: 62, label: "One"},
-    {value: 31, label: "Two"},
-    {value: 17, label: "Three"},
-    {value: 73, label: "Four"},
-    {value: 141, label: "Five"},
-    {value: 4, label: "Six"}
-]
-
-const getSVGElementBoundingBox = (element: SVGElement): DOMRect => {
-    const svgContainer = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-    svgContainer.setAttribute('width', '0');
-    svgContainer.setAttribute('height', '0');
-    svgContainer.appendChild(element)
-    document.body.appendChild(svgContainer)
-    const boundingBox = element.getBoundingClientRect();
-    document.body.removeChild(svgContainer);
-    return boundingBox;
-}
-
-const getRelativeElementPosition = (x: number, y: number, boundingBox: DOMRect, position: Position, padding: number = DEFAULT_ELEMENT_PADDING): [number, number] => {
-    switch (position) {
-        case "left":
-            return [x - (boundingBox.width / 2) - padding, y + (boundingBox.height / 4)]
-        case "right":
-            return [x + (boundingBox.width / 2) + padding, y + (boundingBox.height / 4)]
-        case "top":
-            return [x - (boundingBox.width / 2), y - padding]
-        case "bottom":
-            return [x - (boundingBox.width / 2), y + (boundingBox.height / 4) + padding]
-        default:
-            return [0, 0]
-    }
-}
-
-const drawLabel = (container: SVGElement, x: number, y: number, label: string, labelPosition: Position = "left") => {
-    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    text.textContent = label;
-    const textBoundingBox = getSVGElementBoundingBox(text)
-    const [textX, textY] = getRelativeElementPosition(x, y, textBoundingBox, labelPosition)
-    text.setAttribute('x', textX.toString())
-    text.setAttribute('y', textY.toString())
-    container.appendChild(text)
-}
-
-const drawLine = (container: SVGElement, startX: number, startY: number, rotationDegrees: number, lengthPixels: number, options?: LineOptions) => {
-    const {label, labelPosition, hexColor} = options ?? {}
-    const line = document.createElementNS("http://www.w3.org/2000/svg", "line")
-
-    // calculate end position based on rotation and length
-    const rotationRadians = rotationDegrees * (Math.PI / 180)
-    const endX = startX + lengthPixels * Math.cos(rotationRadians)
-    const endY = startY + lengthPixels * Math.sin(rotationRadians)
-    const roundedEndX = Math.abs(endX) < 1e-10 ? 0 : endX
-    const roundedEndY = Math.abs(endY) < 1e-10 ? 0 : endY
-
-    line.setAttribute("x1", startX.toString())
-    line.setAttribute("y1", startY.toString())
-    line.setAttribute("x2", roundedEndX.toString())
-    line.setAttribute("y2", roundedEndY.toString())
-    line.style.stroke = hexColor ?? "black"
-    line.style.strokeWidth = "1px"
-    container.appendChild(line)
-    if (label) {
-        drawLabel(container, startX, startY, label, labelPosition)
-    }
-    return {element: line, startX, endX: roundedEndX, startY, endY: roundedEndY}
-}
-
-const drawRectangle = (container: SVGElement, x: number, y: number, height: number, width: number, options?: RectangleOptions) => {
-    const {label, labelPosition, hexColor} = options ?? {}
-    const rectangle = document.createElementNS("http://www.w3.org/2000/svg", "rect")
-    rectangle.setAttribute("x", x.toString())
-    rectangle.setAttribute("y", y.toString())
-    rectangle.setAttribute("height", height.toString())
-    rectangle.setAttribute("width", width.toString())
-    rectangle.style.fill = hexColor ?? "red"
-    container.appendChild(rectangle)
-    if (label) {
-        drawLabel(container, x + (width / 2), y, label, labelPosition)
-    }
-    return {element: rectangle, x, y}
-}
-
-const getNiceValue = (uglyValue: number): number => {
-    let niceValue = Math.pow(10, Math.floor(Math.log10(uglyValue)));
-    if (uglyValue > niceValue * 2) {
-        niceValue *= 2;
-    } else if (uglyValue > niceValue * 5) {
-        niceValue *= 5;
-    }
-    return niceValue
-}
-
-const getCenteredPosition = (basePosition: number, axis: "x" | "y", dimension: number) => {
-    const offset = dimension / 2
-    return axis === "x" ? basePosition - offset : basePosition + offset
-}
-
-const drawBarChart = (container: HTMLElement, data: BarChartData[]) => {
-    const svgContainer = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-    svgContainer.setAttribute("height", SVG_HEIGHT.toString())
-    svgContainer.setAttribute("width", SVG_HEIGHT.toString())
-    svgContainer.setAttribute("overflow", "visible")
-
-    // Calculate min/max value and range
-    const values = data.map((e) => e.value);
-    const minValue = Math.min(0, ...values); // Supports negative minimum values
-    const maxValue = Math.max(...values);
-    const range = maxValue - minValue;
-
-    // Calculate tick interval
-    const tickInterval = Math.ceil(range / MAX_TICK_COUNT);
-    const niceTickInterval = getNiceValue(tickInterval);
-    const niceMaxValue = Math.ceil(maxValue / niceTickInterval) * niceTickInterval;
-    const niceMinValue = Math.floor(minValue / niceTickInterval) * niceTickInterval;
-    const niceRange = niceMaxValue - niceMinValue
-
-    const verticalTickCount = Math.ceil(niceRange / niceTickInterval)
-    const horizontalTickCount = data.length
-
-    // draw y-axis value indicators first, so they are in the background
-    for (let i = 1; i < verticalTickCount; i++) {
-        const indicatorValue = i * niceTickInterval;
-        const indicatorY = SVG_HEIGHT - ((indicatorValue - niceMinValue) / niceRange) * SVG_HEIGHT;
-
-        const tickValueOptions: LineOptions = {hexColor: VALUE_INDICATOR_COLOR}
-        drawLine(svgContainer, 0, indicatorY, 0, SVG_HEIGHT, tickValueOptions);
-    }
-
-    // draw bars second, so they are in the middle
-    for (let i = 0; i < horizontalTickCount; i++) {
-        const {value} = data[i]
-
-        const barSpacing = SVG_HEIGHT / (horizontalTickCount + 1);
-        const barHeight = ((value - niceMinValue) / niceRange) * SVG_HEIGHT;
-
-        const barX = getCenteredPosition((i + 1) * barSpacing, "x", BAR_WIDTH)
-        const barY = SVG_HEIGHT - barHeight
-
-        const barOptions: RectangleOptions = {label: value.toString(),labelPosition: "top"}
-        drawRectangle(svgContainer, barX, barY, barHeight, BAR_WIDTH, barOptions)
-    }
-
-    // draw axis', ticks and tips, so they are on top
-    const xAxis = drawLine(svgContainer, 0, SVG_HEIGHT, 0, SVG_HEIGHT)
-    const yAxis = drawLine(svgContainer, 0, SVG_HEIGHT, -90, SVG_HEIGHT)
-
-    // draw x-axis tip
-    drawLine(svgContainer, xAxis.endX, xAxis.endY, 135, 10)
-    drawLine(svgContainer, xAxis.endX, xAxis.endY, -135, 10)
-
-    // draw y-axis tip
-    drawLine(svgContainer, yAxis.endX, yAxis.endY, 45, 10)
-    drawLine(svgContainer, yAxis.endX, yAxis.endY, 135, 10)
-
-    // draw y-axis ticks
-    for (let i = 1; i < verticalTickCount; i++) {
-        const tickValue = i * niceTickInterval;
-
-        const tickX = getCenteredPosition(0, "x", TICK_LENGTH)
-        const tickY = SVG_HEIGHT - ((tickValue - niceMinValue) / niceRange) * SVG_HEIGHT;
-
-        const tickOptions: LineOptions = {label: tickValue.toString(), labelPosition: "left"}
-        drawLine(svgContainer, tickX, tickY, 0, TICK_LENGTH, tickOptions);
-    }
-
-    // draw x-axis ticks (vertical)
-    for (let i = 0; i < horizontalTickCount; i++) {
-        const {label} = data[i]
-        const tickSpacing = SVG_HEIGHT / (horizontalTickCount + 1);
-
-        const tickX = (i + 1) * tickSpacing
-        const tickY = getCenteredPosition(SVG_HEIGHT, "y", TICK_LENGTH)
-
-        const tickOptions: LineOptions = {label, labelPosition: "bottom"}
-        drawLine(svgContainer, tickX, tickY, -90, TICK_LENGTH, tickOptions)
-    }
-
-    container.appendChild(svgContainer)
-}
-
-const doDraw = () => {
-    const container = document.getElementById("container")
-    if (!container) {
-        console.log("no container?? gonna cry??")
+const initAddButton = () => {
+    const addButtonInput = document.getElementById("add-button-input") as HTMLInputElement
+    const buttonContainer = document.getElementById("button-container")
+    if (!addButtonInput || !buttonContainer) {
         return
     }
-    drawBarChart(container, BAR_CHART_DATA)
+    addButtonInput.addEventListener("change", () => {
+        if (!addButtonInput.files) {
+            return
+        }
+        const filesArray = Array.from(addButtonInput.files)
+        for (const file of filesArray) {
+            const imageButton = initImageButton(file)
+            buttonContainer.appendChild(imageButton)
+        }
+    })
+
+    const testFile = initTestFile()
+    const imageButton = initImageButton(testFile)
+    buttonContainer.appendChild(imageButton)
 }
 
-doDraw()
+const initTestFile = () => {
+    const fileContent = atob(PEEPO_CLAP)
+    const buffer = new ArrayBuffer(fileContent.length)
+    const view = new Uint8Array(buffer)
+    for (let i = 0; i < fileContent.length; i++) {
+        view[i] = fileContent.charCodeAt(i);
+    }
+    return new File([buffer], "peepoclap.gif", {type: "image/gif"})
+}
+
+const initImageButton = (file: File): HTMLDivElement => {
+    const container = document.createElement("div")
+    container.classList.add("image-button-container")
+    container.draggable = false
+
+    container.addEventListener("drag", (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+    })
+
+    container.addEventListener("mousedown", () => {
+        isDragging = true
+        draggedElementObjectURL = URL.createObjectURL(file)
+    })
+
+    const div = document.createElement("div")
+    const backgroundImageObjectURL = URL.createObjectURL(file)
+    div.style.background = `url(${backgroundImageObjectURL}) center center / contain no-repeat var(--ternary-color)`
+    div.classList.add("image-button")
+    div.draggable = false
+
+    div.addEventListener("drag", (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+    })
+
+    container.appendChild(div)
+    return container
+}
+
+const createDraggableElement = (objectURL: string, initialXPosition: number, initialYPosition: number) => {
+    const dragged = document.createElement("div")
+    dragged.setAttribute("id", "dragged")
+    dragged.style.left = `calc(${initialXPosition}px - 5rem)`
+    dragged.style.top = `calc(${initialYPosition}px - 5rem)`
+
+    const draggedImage = document.createElement("div")
+    draggedImage.setAttribute("id", "dragged-img")
+    draggedImage.style.background = `url(${objectURL}) center center / contain no-repeat var(--ternary-color)`
+    dragged.appendChild(draggedImage)
+
+    const container = document.getElementById("draggable-container")
+    if (!container) {
+        return null
+    }
+    container.appendChild(dragged)
+}
+
+const createTierContentPlaceholder = (tierContent: Element) => {
+    const existingPlaceholder = [...tierContent.children].find((e) => e.id === "tier-content-placeholder")
+    if (existingPlaceholder) {
+        return
+    }
+    const placeholder = document.createElement("div")
+    placeholder.setAttribute("id", "tier-content-placeholder")
+    tierContent.appendChild(placeholder)
+}
+
+const destroyTierContentPlaceholder = (tierContent: Element) => {
+    const placeholder = [...tierContent.children].find((e) => e.id === "tier-content-placeholder")
+    if (placeholder) {
+        tierContent.removeChild(placeholder)
+    }
+}
+
+const createTierContentItem = (tierContent: Element, objectURL: string) => {
+    const tierContentItem = document.createElement("div")
+    tierContentItem.classList.add("tier-content-item")
+    tierContentItem.style.background = `url(${objectURL}) center center / contain no-repeat var(--ternary-color)`
+
+    const placeholder = [...tierContent.children].find((e) => e.id === "tier-content-placeholder")
+    if (!placeholder) {
+        return
+    }
+    tierContent.replaceChild(tierContentItem, placeholder)
+}
+
+const resetDraggingProperties = () => {
+    const container = document.getElementById("draggable-container")
+    const draggedElement = document.getElementById("dragged")
+    if (container && draggedElement) {
+        container.removeChild(draggedElement)
+    }
+
+    if (draggedElementObjectURL) {
+        URL.revokeObjectURL(draggedElementObjectURL)
+    }
+    draggedElementObjectURL = null
+
+    isDragging = false
+}
+
+const initDragging = () => {
+    window.addEventListener("mouseup", () => {
+        if (!isDragging) {
+            resetDraggingProperties()
+            return
+        }
+
+        const tierContentPlaceholder = document.getElementById("tier-content-placeholder")
+        const tierContent = tierContentPlaceholder?.parentElement
+        if (!tierContent) {
+            resetDraggingProperties()
+            return
+        }
+
+        if (draggedElementObjectURL) {
+            createTierContentItem(tierContent, draggedElementObjectURL)
+        } else {
+            destroyTierContentPlaceholder(tierContent)
+        }
+
+        resetDraggingProperties()
+    })
+
+    window.addEventListener("mousemove", (e) => {
+        if (!isDragging || !draggedElementObjectURL) {
+            return
+        }
+        const draggedElement = document.getElementById("dragged")
+        if (!draggedElement) {
+            createDraggableElement(draggedElementObjectURL, e.clientX, e.clientY)
+            return
+        }
+        draggedElement.style.left = `calc(${e.clientX}px - 5rem)`
+        draggedElement.style.top = `calc(${e.clientY}px - 5rem)`
+
+        const tierContents = document.getElementsByClassName("tier-content")
+        for (const tierContent of tierContents) {
+            const tierContentBoundingRect = tierContent.getBoundingClientRect()
+
+            const doesXOverlap = e.clientX > tierContentBoundingRect.x && e.clientX < tierContentBoundingRect.x + tierContentBoundingRect.width
+            const doesYOverlap = e.clientY > tierContentBoundingRect.y && e.clientY < tierContentBoundingRect.y + tierContentBoundingRect.height
+
+            if (doesXOverlap && doesYOverlap) {
+                createTierContentPlaceholder(tierContent)
+            } else {
+                destroyTierContentPlaceholder(tierContent)
+            }
+        }
+    })
+}
+
+initAddButton()
+initDragging()
